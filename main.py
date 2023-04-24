31530826c5fcd05f5e988607e514004b6ba7db75
mrkicrade <mrkic78@gmail.com>
2023-04-24 19:11:12 +0200

    Create columns, image and description.

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('images/photo.jpg')
+
+with col2:
+    st.tile('Mrkić Rade')
+    content = """
+    Zdravo. Ja sam Mrkić Rade i ja sam Python developer i educator web dizajna i web programiranja. Radim kao asistent i saradnik u 
+    on line školi programiranja "Danilo Vešović. 
+    """
+    st.info(content)
