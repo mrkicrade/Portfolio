d3f397b2d61c19a93d1dc99ab34c7751ad08f919
mrkicrade <mrkic78@gmail.com>
2023-04-26 15:08:31 +0200

    Day 23: Add Email form.

--- send_email.py
+++ send_email.py
@@ -1,19 +1,21 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "mrkic78@gmail.com"
-password = "aczcvczgcgdklnzy"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "mrki78@gmail.com"
-context = ssl.create_default_context()
+    username = "mrkic78@gmail.com"
+    password = "aczcvczgcgdklnzy"
 
-message = """
-Hi!
-How are you?
-Bye!
-"""
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    receiver = "mrkic78@gmail.com"
+    context = ssl.create_default_context()
+
+    message = """
+    Hi!
+    How are you?
+    Bye!
+    """
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
